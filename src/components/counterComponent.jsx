76273408c45d25bbafb6a6473dffcc1432ebfae4
anishdass <anishdassatoffice@gmail.com>
2024-08-28 00:20:22 +0100

    Incrementer added

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </div>
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
+      </>
     );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import Counter from './components/counterComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Counter />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
